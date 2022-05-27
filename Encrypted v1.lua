bb986e7b72f8e8d83de2fc1b8ab4db99d440e66f
iMrt <90536579+iMrtwiin@users.noreply.github.com>
2022-05-27 20:59:53 -0300

    Create Encrypted v1.lua

---
+++ Encrypted v1.lua
@@ -0,0 +1,7 @@
+game.StarterGui:SetCore("SendNotification",  {
+ Title = "Encrypted";
+ Text = "Encrypted is Still In Developement";
+ Icon = "rbxassetid://00000000000";
+ Duration = 60;
+ Callback = NotificationBindable;
+})
