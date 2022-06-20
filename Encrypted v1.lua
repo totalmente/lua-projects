3ca84c117958e26738a752199023e145c0815fbb
iMrt <90536579+iMrtwiin@users.noreply.github.com>
2022-06-20 15:58:27 -0300

    Delete Encrypted v1.lua

--- Encrypted v1.lua
+++
@@ -1,7 +0,0 @@
-game.StarterGui:SetCore("SendNotification",  {
- Title = "Encrypted";
- Text = "Encrypted is Still In Developement";
- Icon = "rbxassetid://00000000000";
- Duration = 60;
- Callback = NotificationBindable;
-})
